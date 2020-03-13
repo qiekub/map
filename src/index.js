e8535a4f9dc7bea061504c53ad0d891b6bd018bc
Thomas Rosen <thomas.rosen@me.com>
2020-03-13 11:07:41 +0100

    backup-commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App/index.js'
+
+import ApolloClient from 'apollo-boost'
+window.graphql = new ApolloClient({
+	uri: 'http://localhost:5001/queercenters/us-central1/graphql/v1',
+})
+window.pageOpenTS = new Date()
+
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
