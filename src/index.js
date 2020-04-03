f61e68bc8ed0e3f9a553221019abfe1feb5b264a
Thomas Rosen <thomas.rosen@me.com>
2020-04-03 05:52:59 +0200

    Started @fluent support

--- src/index.js
+++ src/index.js
@@ -2,20 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+import 'intl-pluralrules'
+import { AppLocalizationProvider } from './l10n.js';
+
 import local_ip from './.env.local_ip.json'
 
 // import * as serviceWorker from './serviceWorker'
 
-import App from './App/index.js'
+import App from './components/App/index.js'
 
 import ApolloClient from 'apollo-boost'
 // import { InMemoryCache } from 'apollo-cache-inmemory';
-
 // const cache = new InMemoryCache()
 
 window.graphql = new ApolloClient({
 	// cache,
-
 	uri: (
 		local_ip !== ''
 		? `http://${local_ip}:5000/queercenters/us-central1/graphql/graphql/v1`
@@ -25,7 +26,12 @@ window.graphql = new ApolloClient({
 
 window.pageOpenTS = new Date()
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+	<AppLocalizationProvider userLocales={navigator.languages}>
+		<App />
+	</AppLocalizationProvider>,
+	document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
