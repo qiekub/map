3bccbc68ef4220551c1b9ab31cc04a4711a38c59
Thomas Rosen <thomas.rosen@me.com>
2020-04-21 23:27:00 +0200

    disabled eslint for a line

--- src/functions.js
+++ src/functions.js
@@ -2,6 +2,7 @@ export function uuidv4() {
 	// source: https://stackoverflow.com/a/2117523/2387277
 	// source: https://stackoverflow.com/questions/105034/how-to-create-guid-uuid
 	return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+		// eslint-disable-next-line
 		(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
 	)
 }
