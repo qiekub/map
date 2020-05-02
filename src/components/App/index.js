5ae55397f1a51c8e7845a769e05038750b422b55
Thomas Rosen <thomas.rosen@me.com>
2020-05-02 13:21:10 +0200

    added an idea for a cookie banner

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -381,6 +381,10 @@ class App extends React.Component {
 							}}
 						></Localized>
 					</Typography>
+
+					{/*
+						We're saving some data on your computer. This only data like the map position. We're gonna ask you for permission before saving identifying data that is send to our servers.
+					*/}
 				</CardContent>
 				<CardActions>
 					<Button onClick={this.closeIntro}>
