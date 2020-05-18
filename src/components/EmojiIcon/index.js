71a3ff903274060fe457389dcbf7673039049d98
Thomas Rosen <thomas.rosen@me.com>
2020-05-18 15:43:52 +0200

    added the icon-name as the default key

--- src/components/EmojiIcon/index.js
+++ src/components/EmojiIcon/index.js
@@ -3,14 +3,17 @@ import React from 'react'
 export default class EmojiIcon extends React.Component {
 	render() {
 		return (
-			<div style={{
-				width: '1em',
-				height: '1em',
-				display: 'inline-block',
-				fontSize: '1.5rem',
-				lineHeight: '1em',
-				textAlign: 'center',
-			}}>
+			<div
+				key={this.props.key || this.props.icon}
+				style={{
+					width: '1em',
+					height: '1em',
+					display: 'inline-block',
+					fontSize: '1.5rem',
+					lineHeight: '1em',
+					textAlign: 'center',
+				}}
+			>
 				{this.props.icon}
 			</div>
 		)
