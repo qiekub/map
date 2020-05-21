2fb6b148add27d6e492eca668f84579f7b8f2866
Thomas Rosen <thomas.rosen@me.com>
2020-05-21 19:09:29 +0200

    moved into sepperate lines

--- src/components/Questions/index.js
+++ src/components/Questions/index.js
@@ -687,7 +687,6 @@ class Questions extends React.Component {
 								return undefined
 							}else{
 								const possibleAnswerKey = possibleAnswer.key
-								const possibleAnswerTags = possibleAnswer.tags || {}
 								const possibleAnswerNamespace = possibleAnswer.namespace
 								possibleAnswer.inputtype = possibleAnswer.inputtype || ''
 								if (possibleAnswer.inputtype === 'date') {
@@ -757,7 +756,10 @@ class Questions extends React.Component {
 										<ListItem
 											button
 											key={possibleAnswerKey}
-											onClick={()=>this.answerQuestion(questionDoc._id, possibleAnswerTags)}
+											onClick={() => this.answerQuestion(
+												questionDoc._id,
+												possibleAnswer.tags || {},
+											)}
 											variant="outlined"
 											size="large"
 											style={{
