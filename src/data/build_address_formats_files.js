eefa57a7fd1339ededf490942f2e5619e43eddd0
Thomas Rosen <thomas.rosen@me.com>
2020-08-28 23:32:56 +0200

    removed dropdown infos and added an array with all-address-fields to address_formats.json

--- src/data/build_address_formats_files.js
+++ src/data/build_address_formats_files.js
@@ -1,11 +1,42 @@
 const fs = require('fs')
 
 let id_address_formats = require('./iD/data/address_formats.json')
-id_address_formats = id_address_formats.map(item => ({
-	...item,
-	countryCodes: (item.countryCodes || []).map(item => item.toUpperCase()),
-	// keys: [].concat(...item.format),
-}))
+id_address_formats = id_address_formats.map(item => {
+	delete item.dropdowns
+	return {
+		...item,
+		countryCodes: (item.countryCodes || []).map(item => item.toUpperCase()),
+		// keys: [].concat(...item.format),
+	}
+})
+
+id_address_formats.push({
+	countryCodes: ['_ALL_FIELDS_'],
+	format: [
+		['housename'],
+		['street', 'housenumber'],
+		['postcode', 'city'],
+
+		['hamlet'],
+		['suburb'],
+		['subdistrict'],
+		['district'],
+		['province'],
+		['state'],
+
+		['quarter'],
+		['neighbourhood'],
+		// ['place'],
+
+		// ['door'],
+		['unit'],
+		// ['flats'],
+		['floor'],
+		['block_number'], // ['block'],
+		// ['conscriptionnumber'],
+	],
+	width: null
+})
 
 const distPath = './dist/'
 if (!fs.existsSync(distPath)){
